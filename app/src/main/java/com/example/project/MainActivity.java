3a65e64f6a00493adfa9be880dcbcd4b4cc4f573
b23ebbst <b23ebbst@his.student.se>
2024-05-03 10:50:08 +0200

    Fixat till så att angiven text i second_activity visas i activity_main.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +23,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button b = findViewById(R.id.button_main);
+        myPreferenceRef = getSharedPreferences("MyAppPreferenceString", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
+        TextView text_view = findViewById(R.id.text_view);
+
+        Button b = findViewById(R.id.button_main);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
             }
-
-
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        myPreferenceRef = getSharedPreferences("MyAppPreferenceString", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        // Read a preference
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.text_view);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+    }
 }
--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -3,6 +3,9 @@ package com.example.project;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +20,37 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
 
-        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceRef = getSharedPreferences("MyAppPreferenceString", MODE_PRIVATE);
         myPreferenceEditor = myPreferenceRef.edit();
 
+        TextView prefText = findViewById(R.id.prefText);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            prefText.setText(name);
+        }
+        // Read a preference
         TextView prefTextRef=new TextView(this);
         prefTextRef=(TextView)findViewById(R.id.prefText);
         prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+    }
+
+    public void savePref(View v){
+        // Get the text
+        EditText newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.settingseditview);
+
+        // Store the new preference
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        // Display the new preference
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+        // Clear the EditText
+        newPrefText.setText("");
     }
 }
